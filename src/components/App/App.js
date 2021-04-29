c92cc2e41b799250e076a4dcf49ff5d14c557a9d
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-29 15:26:30 -0400

    routes setup with browser router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from '../LandingPage/LandingPage';
+import NavBar from '../NavBar/NavBar';
+import About from '../About/About';
+import Portfolio from '../Portfolio/Portfolio';
+import Contact from '../Contact/Contact';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route component={LandingPage} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={Portfolio} path="/portfolio" />
+        <Route component={Contact} path="/contact" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;

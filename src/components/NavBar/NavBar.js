dfd6dcdf6be51b4b52344c435c15dd431d8fc330
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-05-02 19:53:08 -0400

    react bootstrap nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import NavBarCSS from './NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Thomas Wallace</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
